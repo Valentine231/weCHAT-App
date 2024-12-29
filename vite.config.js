8d1eb54254d05f44f7f4cfe4e385cd418d517de9
valentine231 <ugwuvalentine519@gmail.com>
2024-12-29 16:22:47 +0100

    small-changes

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
     },
   },
   define: {
+    global: 'globalThis',
     'process.env': JSON.stringify({ NODE_ENV: process.env.NODE_ENV || 'development' }), // Avoid exposing all variables; set to empty or only used by polyfills
     'import.meta.env': JSON.stringify(exposedEnv), // Expose only selected variables
   },
